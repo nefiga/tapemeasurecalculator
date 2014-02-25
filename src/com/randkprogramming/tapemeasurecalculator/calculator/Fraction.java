1427afd4a1c3c2a1eb547101ba3a86ec084a94a3
Jason Robertson <jayperdu@gmail.com>
2014-02-24 19:55:39 -0700

    preparation for rounding

---
+++ src/com/randkprogramming/tapemeasurecalculator/calculator/Fraction.java
@@ -0,0 +1,322 @@
+package com.randkprogramming.tapemeasurecalculator.calculator;
+
+/**
+ * A simple class for storing fractions and performing simple arithmetic operations on them.
+ * @author Apollo
+ */
+public class Fraction {
+	
+	@SuppressWarnings("serial")
+	public class ZeroDenominatorException extends Exception {
+	}
+	
+	//--------------------
+	// Members
+	//--------------------
+	private int numerator;
+	private int denominator;
+	
+	//--------------------
+	// Getters
+	//--------------------
+	/** @return The numerator value of the fraction. */
+	public int getNumerator() { return this.numerator; }
+	
+	/** @return The denominator value of the fraction. */
+	public int getDenominator() { return this.denominator; }
+	
+	/** @return The result of numerator / denominator */
+	public double getDecimalRepresentation() {
+		double n = (double) this.numerator;
+		double d = (double) this.denominator;
+		return n/d;
+	}
+	
+	/** @return A new fraction that is the reciprocal of the given fraction. (The numerator and denominator switch places) */
+	public Fraction getReciprocal() throws ZeroDenominatorException {
+		return new Fraction(this.denominator, this.numerator);
+	}
+	
+	/**
+	 * @param decimal The decimal you want to convert.
+	 * @param precision Rounds the answer to the closest given fraction.
+	 *      e.g. if you give 1/16 as the round value you will get
+	 *           an answer that is a multiple of 1/16 in simple reduced form.
+	 * @return The resulting fraction in reduced form.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException if the denominator of the round parameter you provide is 0.
+	 */
+	public static Fraction getFractionFromDecimal(double decimal, Fraction precision) throws ZeroDenominatorException {
+		double precisionValue = precision.getDecimalRepresentation();
+		double roundOffValue = precisionValue / 2;
+		
+		int numerator = (int) ( (decimal + roundOffValue) / precisionValue );
+		int denominator = precision.getDenominator();
+		return new Fraction(numerator,denominator);
+	}
+	
+	public static Fraction getFractionFromString(String fraction_string) throws ZeroDenominatorException {
+		 
+		StringBuilder sb = new StringBuilder();
+		int numerator = 0;
+		int denominator = 0;
+		
+		int i = 0;
+		char c = fraction_string.charAt(i);
+		
+		while(c != '/') {
+			sb.append(c);
+			i++;
+			c = fraction_string.charAt(i);
+		}
+		
+		numerator = Integer.parseInt(sb.toString());
+		denominator = Integer.parseInt(fraction_string.substring(i+1));
+			
+		return new Fraction(numerator,denominator);
+	}
+	
+	//--------------------
+	// Set Values
+	//--------------------
+	/**
+	 * Sets the values of the numerator and denominator respectively. Negative values are acceptable
+	 * for either one, or both. Denominator can not be 0.
+	 * @param n numerator
+	 * @param d denominator
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	private void setValues(int n, int d) throws ZeroDenominatorException {
+		
+		if(d == 0)
+			throw new ZeroDenominatorException();
+		
+		if(d < 0) {
+			n *= -1;
+			d *= -1;
+		}
+		
+		this.numerator = n;
+		this.denominator = d;
+		
+	}
+	
+	//--------------------
+	// Constructors
+	//--------------------
+	/** Empty constructor, does nothing. */
+	public Fraction() {}
+	
+	/**
+	 * This constructor works much like a copy or clone method. It returns a new Fraction that is equal to the given fraction, and reduces the result if necessary.
+	 * @param f fraction
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public Fraction(Fraction f) throws ZeroDenominatorException {
+		setValues(f.numerator,f.denominator);
+		simplify();
+	}
+	
+	/**
+	 * Creates a new Fraction from the given parameters and reduces the result.
+	 * @param n numerator
+	 * @param d denominator
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public Fraction(int n, int d) throws ZeroDenominatorException {
+		setValues(n,d);
+		simplify();
+	}
+	
+	
+	//--------------------
+	// Add
+	//--------------------
+	/**
+	 * The result of adding the two fractions together.
+	 * This method does not modify the original fractions.
+	 * @return a new Fraction containing the result in simplest form.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public static Fraction add(final Fraction leftSide, final Fraction rightSide) throws ZeroDenominatorException {
+		
+		Fraction ls = new Fraction(leftSide);
+		Fraction rs = new Fraction(rightSide);
+
+		findCommonDenominator(ls,rs);
+		
+		int n = ls.numerator + rs.numerator;
+		int d = ls.denominator;
+		
+		return new Fraction(n,d);
+		
+	}
+	
+	
+	//--------------------
+	// Subtract
+	//--------------------
+	/**
+	 * The result of subtracting the right side from the left. 
+	 * This method does not modify the original fractions.
+	 * @return a new Fraction containing the result in simplest form.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public static Fraction subtract(final Fraction leftSide, final Fraction rightSide) throws ZeroDenominatorException {
+		
+		Fraction ls = new Fraction(leftSide);
+		Fraction rs = new Fraction(rightSide);
+
+		findCommonDenominator(ls, rs);
+		
+		int n = ls.numerator - rs.numerator;
+		int d = ls.denominator;
+		
+		return new Fraction(n,d);
+		
+	}
+	
+	
+	//----------------------------
+	// Find Common Denominator
+	//----------------------------
+	/**
+	 * Modifies both fractions so that the denominators are equal. This function is called before adding and subtracting fractions.
+	 * Specifically, the denominators are multiplied together to find the new denominator, and the numerators are multiplied
+	 * by the corresponding ratio.
+	 */
+	private static void findCommonDenominator(Fraction leftSide, Fraction rightSide) {
+		if(leftSide.denominator != rightSide.denominator) {
+			int commonDenominator = leftSide.denominator * rightSide.denominator;
+
+			leftSide.numerator = leftSide.numerator * rightSide.denominator;
+			rightSide.numerator = rightSide.numerator * leftSide.denominator;
+
+			leftSide.denominator = commonDenominator;
+			rightSide.denominator = commonDenominator;	
+		}
+	}
+	
+	
+	//--------------------
+	// Multiply
+	//--------------------
+	/**
+	 * Multiplies numerators together and denominators together and returns a new fraction as the result.
+	 * Does not modify the original fractions.
+	 * @return a new Fraction containing the result in simplest form.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public static Fraction multiply(final Fraction leftSide, final Fraction rightSide) throws ZeroDenominatorException {
+		int n = leftSide.numerator * rightSide.numerator;
+		int d = leftSide.denominator * rightSide.denominator;
+		return new Fraction(n,d);
+	}
+
+	
+	//--------------------
+	// Divide
+	//--------------------
+	/**
+	 * Does not modify the fractions. It returns a new fraction as the result.
+	 * Dividing a fraction by another fraction yields the same result as multiplying
+	 * the fraction by the other fraction's reciprocal (3/4 DIVIDE 2/3 == 3/4 MULTIPLY 3/2)
+	 * @return a new Fraction containing the result in simplest form.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public static Fraction divide(final Fraction leftSide, final Fraction rightSide) throws ZeroDenominatorException {
+		int n = leftSide.numerator * rightSide.denominator;
+		int d = leftSide.denominator * rightSide.numerator;
+		return new Fraction(n,d);
+	}
+	
+	
+	//--------------------
+	// Simplify
+	//--------------------
+	/**
+	 * Reduces a fraction to its simplest form by finding the greatest common divisor of the numerator and denominator
+	 * and then divides each one by that number. If there is none, the result stays the same.
+	 * @throws com.randkprogramming.tapemeasurecalculator.calculator.Fraction.ZeroDenominatorException
+	 */
+	public void simplify() throws ZeroDenominatorException {
+		int greatestCommonDivisor = findGCD(this.numerator,this.denominator);
+		this.numerator /= greatestCommonDivisor;
+		this.denominator /= greatestCommonDivisor;
+	}
+	
+	
+	//--------------------
+	// Find GCD
+	//--------------------
+	/** Finds the greatest common divisor of two integers. If there is none, the result is 1. */
+	public static int findGCD(int a, int b) {
+		if (b == 0)
+			return a;
+		else
+			return findGCD(b, a % b);
+	}
+	
+	
+	//--------------------
+	// To String
+	//--------------------
+	/** Returns a string representation of the fraction. */
+	@Override
+	public String toString() {
+		int wholeNumber = numerator / denominator;
+		int displayedNumerator = 0;
+		if(wholeNumber > 0) {
+			displayedNumerator = numerator - (wholeNumber * denominator);
+			if(displayedNumerator == 0)
+				return wholeNumber + "";
+			else
+				return wholeNumber + " " + displayedNumerator + "/" + denominator;
+		}
+		
+		return numerator + "/" + denominator;
+	}
+	
+	
+	//--------------------
+	// Hash Code
+	//--------------------
+	/** Returns a hashcode value for the fraction */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + denominator;
+		result = prime * result + numerator;
+		return result;
+	}
+	
+	
+	//--------------------
+	// Equals
+	//--------------------
+	/** Checks to see whether two fractions are equal or not. A fraction
+	 * is equal if both the numerator and denominator are the same when in reduced form.*/
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		
+		Fraction other = (Fraction) obj;
+		
+		try {
+			Fraction leftSide = new Fraction(this);
+			Fraction rightSide = new Fraction(other);
+			
+			leftSide.simplify();
+			rightSide.simplify();
+
+			if (leftSide.numerator != rightSide.numerator) return false;
+			if (leftSide.denominator != rightSide.denominator) return false;
+			
+		} catch(ZeroDenominatorException e) { e.printStackTrace(); }
+		
+		return true;
+	}
+	
+}
--- src/com/randkprogramming/tapemeasurecalculator/calculator/mechanics/CalculateEquation.java
+++ src/com/randkprogramming/tapemeasurecalculator/calculator/mechanics/CalculateEquation.java
@@ -106,11 +106,9 @@ public class CalculateEquation {
      * Sets operators[] int, at position, to default. Also subtracts one from operatorPosition.
      */
     public void decreesOperatorPosition() {
+        operators[operatorPosition] = CalculatorButtons.NONE;
         if (operatorPosition > 0) {
-            operators[operatorPosition] = CalculatorButtons.NONE;
             operatorPosition--;
-        } else {
-            operators[operatorPosition] = CalculatorButtons.NONE;
         }
     }
 
@@ -262,21 +260,41 @@ public class CalculateEquation {
                 }
             }
 
-            numbers[0] = numbers[numberPosition];
+            numbers[0] = roundAnswer(numbers[numberPosition],fractionButton);
+
             resetNumbers(1);
             resetOperators(0);
             number = Double.toString(numbers[0]);
-            if (fractionButton == CalculatorButtons.SIXTEENTH) {
-                equation = Double.toString(numbers[0]);
-            } else if (fractionButton == CalculatorButtons.THIRTYSECOND) {
+            equation = Double.toString(numbers[0]);
 
-            } else if (fractionButton == CalculatorButtons.SIXTYFOURTH) {
 
-            } else if (fractionButton == CalculatorButtons.DECIMAL) {
-
-            } else {
-
-            }
         }
     }
+
+    /**
+     * Rounds the answer according to the selected fraction button
+     * @param unrounded_answer The answer that needs to be rounded
+     * @param fractionButton The selected fraction button that the user currently has selected
+     * @return The rounded answer
+     */
+    public double roundAnswer(double unrounded_answer, int fractionButton) {
+
+        // TODO: Finish this method
+
+        if (fractionButton == CalculatorButtons.SIXTEENTH) {
+
+        } else if (fractionButton == CalculatorButtons.THIRTYSECOND) {
+
+        } else if (fractionButton == CalculatorButtons.SIXTYFOURTH) {
+
+        } else if (fractionButton == CalculatorButtons.DECIMAL) {
+
+        } else {
+
+        }
+
+        return 0;
+    }
+
+
 }
