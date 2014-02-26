6126433da1a7c18f63a0a5d026db4bfa46548a95
nefiga <thewaronryan@hotmail.com>
2014-02-26 16:23:45 -0700

    adding inbounds method

--- src/com/randkprogramming/tapemeasurecalculator/calculator/FractionSixteenthScreen.java
+++ src/com/randkprogramming/tapemeasurecalculator/calculator/FractionSixteenthScreen.java
@@ -4,6 +4,7 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import com.randkprogramming.tapemeasurecalculator.Calculator;
 import com.randkprogramming.tapemeasurecalculator.Graphics;
+import com.randkprogramming.tapemeasurecalculator.Input;
 import com.randkprogramming.tapemeasurecalculator.Screen;
 import com.randkprogramming.tapemeasurecalculator.calculator.mechanics.CalculatorInputManager;
 
@@ -31,6 +32,12 @@ public class FractionSixteenthScreen extends Screen{
     public void present(float deltaTime) {
         Graphics g = calculator.getGraphics();
         g.drawString("SixteenthScreen", 20, 10, paint);
+    }
+
+    // Checks to see if your finger is within an area
+    public boolean touchIsInBounds(Input.TouchEvent event, int x, int y, int width, int height) {
+        if (event.x > x && event.x < x + width - 1 && event.y > y && event.y < y + height - 1) return true;
+        else return false;
     }
 
     @Override
